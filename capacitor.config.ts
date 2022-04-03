4f9c920e5e15b7b6a0581c794d82924c8ec10dbe
Lehoczki Patrícia <lehoczkipatricia99@gmail.com>
2022-04-03 20:47:17 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fogadj-orokbe',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
